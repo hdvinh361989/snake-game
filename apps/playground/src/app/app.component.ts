9ba68f86a4ef9d71ffb0e6ec3411ffa5f34aceb4
Vinh Hoang <vinh@streethawk.com>
2024-07-16 11:39:11 +0700

    feat: add width and height inputs

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import '@playground/snake-game';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {
-  title = 'playground';
+export class AppComponent implements OnInit {
+  form = new FormGroup({
+    width: new FormControl(5, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(3)],
+    }),
+    height: new FormControl(5, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(3)],
+    }),
+  });
+
+  submitted?: { width: number; height: number };
+
+  ngOnInit(): void {
+    this.submit();
+  }
+
+  submit() {
+    this.submitted = {
+      width: this.form.value.width as number,
+      height: this.form.value.height as number,
+    };
+  }
 }
